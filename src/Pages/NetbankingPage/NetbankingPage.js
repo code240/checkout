52ba950543054f5148b086030532860d8cf8c2cb
Vipin <vipin@paytring.com>
2025-10-15 15:54:24 +0530

    test commit for cicd

--- src/Pages/NetbankingPage/NetbankingPage.js
+++ src/Pages/NetbankingPage/NetbankingPage.js
@@ -1,13 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import "./NetbankingPage.scss";
 import { AppContext } from "../../Contexts/AppProvider";
 import OrderSummary from "../../Components/OrderSummary/OrderSummary";
 import Coupons from "../../Components/Coupons/Coupons";
 import LoginComponent from "../../Components/LoginComponent/LoginComponent";
 import NetBankingPayment from "../../Components/NetBankingPayment/NetBankingPayment";
+import { useLocation } from "react-router-dom";
 
 const NetbankingPage = () => {
     const { setActiveSection } = useContext(AppContext);
+
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
     return (
         <div className="NetbankingPage">
 
